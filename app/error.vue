cdbdaf12932aee744b8a4499b28b9140022225c0
Pedro Pisandelli <pisandelli@gmail.com>
2024-09-13 19:12:05 -0300

    Add default pages

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+defineProps({
+  error: {
+    type: Object as () => NuxtError,
+    required: true
+  }
+})
+</script>
+
+<template lang='pug'>
+
+  h1 {{ error?.statusCode }}
+  pre {{ error?.statusMessage }}
+
+</template>
+<style scoped></style>
