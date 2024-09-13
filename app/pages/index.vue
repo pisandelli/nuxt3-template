76473eaa9230f7ed47e08636c7c35a52d6f0290f
Pedro Pisandelli <pisandelli@gmail.com>
2024-09-13 19:15:39 -0300

    Squashed commit of the following:
    commit cdbdaf12932aee744b8a4499b28b9140022225c0
    Author: Pedro Pisandelli <pisandelli@gmail.com>
    Date:   Fri Sep 13 19:12:05 2024 -0300
    
        Add default pages

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang='ts' setup>
+/**
+* The index page
+* @name 'IndexPage'
+* @version 1.0.0
+*/
+</script>
+
+<template lang="pug">
+  h1 Hello World
+</template>
+
+<style lang="stylus" scoped>
+</style>
